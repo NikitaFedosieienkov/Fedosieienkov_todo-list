5fde56886215ffbf4a1c8c4d0bc76809f4bf85ff
nikitafedosieienkov <feedexx04@gmail.com>
2021-10-15 14:57:18 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { randomColor } from 'randomcolor';
+import Draggable from 'react-draggable';
+
+import './App.css';
+
+function App() {
+
+}
+
+export default App;
